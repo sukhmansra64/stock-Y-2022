1e0d750068a85dc7f54c8e8b056f0daf39df8f35
SukhmanSra <ssra5@uwo.ca>
2022-06-26 22:54:54 -0400

    Update: Finally finished getting the devise stuff working!
    Rails is hard.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :exception if proc { |c| c.request.format != 'application/json' }
+  protect_from_forgery with: :null_session if proc { |c| c.request.format == 'application/json' }
 end
