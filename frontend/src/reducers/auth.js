5d87a720a21332c91b7dc4b0a08780eb0c269938
SukhmanSra <ssra5@uwo.ca>
2022-07-14 22:49:53 -0400

    Update
    Added the authentication reducer, next thing is to set up the auth actions, see how data is received by axios and then redo some of the reducer stuff accordingly.

---
+++ frontend/src/reducers/auth.js
@@ -0,0 +1,54 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    LOAD_USER,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
+} from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+};
+
+const authReducer = (state = initialState, action) =>{
+    const {type,payload} = action;
+    switch (type){
+        case LOAD_USER:
+            return{
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+            localStorage.setItem("token",payload.token);
+            return{
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            }
+        case LOGOUT:
+        case LOGIN_FAIL:
+        case REGISTER_FAIL:
+        case AUTH_ERROR:
+            localStorage.removeItem("token");
+            return{
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
